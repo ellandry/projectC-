fd3198ced553827da66c38fba2b4e151dbc9b584
ellandry <ellandry777@mail.ru>
2022-12-01 12:31:04 +0300

    Make seminar3 homework

---
+++ Seminar3/Task21/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между
+// ними в N-мерном пространстве. Сначала задается N с клавиатуры, потом задаются координаты точек.
+Console.Write("Введите размерность пространства: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1) Console.WriteLine("Введите целое положительное число");
+else
+{
+double[] x = new double[n];
+double[] y = new double[n];
+double square = 0;
+
+try
+{
+    for(int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"Введите координаты точки А[{i+1}] ");
+        x[i] = Convert.ToDouble(Console.ReadLine()); 
+    }
+    for(int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"Введите координаты точки B[{i+1}] ");
+        y[i] = Convert.ToDouble(Console.ReadLine());  
+    }
+    for (int i = 0; i < n; i++)
+    {
+        square = square + Math.Pow((x[i]-y[i]), 2);
+    }
+    double distance = Math.Round(Math.Sqrt(square), 2);
+    Console.WriteLine($"Расстояние между введенными точками составляет: {distance}");
+}
+catch
+{
+    Console.WriteLine("Проверьте правильность ввода данных");
+}
+    Console.WriteLine();
+}
---
+++ Seminar3/Task23/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел 
+//от 1 до N.    5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Напишите целое положительное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n <= 0) Console.WriteLine("Вы ввели отрицательное число или 0");
+else
+{
+Console.WriteLine($"Кубы чисел от 1 до {n} равны: ");    
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)} ");
+}
+}
+Console.WriteLine();
